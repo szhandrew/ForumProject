6bc06f4fdac58f646a332da06d1176cdf9d7a562
zhaohuis <zhaohuis@sfu.ca>
2018-08-04 16:16:09 -0700

    deploy 1st commit

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,29 +1,54 @@
 # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
 cookbook_file "apt-sources.list" do
-	path "/etc/apt/sources.list"
+  path "/etc/apt/sources.list"
 end
 execute 'apt_update' do
-	command 'apt-get update'
+  command 'apt-get update'
 end
+
 
 # Base configuration recipe in Chef.
 package "wget"
 package "ntp"
-package "curl"
-package "python3-pip"
-package "python3-dev"
-package "nodejs"
-# package "postgresql-server-dev-all"
-# package "libpython-dev"
-# package "libpq-dev"
-
 cookbook_file "ntp.conf" do
 	path "/etc/ntp.conf"
 end
 execute 'ntp_restart' do
 	command 'service ntp restart'
 end
+package "curl"
+package "python3-pip"
+package "python3-dev"
+package "nodejs"
 
+
+
+#Postgres
+package "postgresql"
+execute 'postgres_user' do
+	command 'echo "CREATE DATABASE mydb; CREATE USER vagrant; GRANT ALL PRIVILEGES ON DATABASE mydb TO vagrant;" | sudo -u postgres psql'
+end
+
+# nginx
+package "nginx"
+cookbook_file "nginx-default" do
+	path "/etc/nginx/sites-available/default"
+end
+service "nginx" do
+	action :restart
+end
+
+# Django Deploy mode
+package "postgresql-server-dev-all"
+package "libpython3-dev"
+execute 'install django uwsgi' do
+	command 'pip3 install django psycopg2 uwsgi'
+end
+
+
+
+
+#### -------------project dependencies---------------
 execute 'install_django' do
 	command 'pip3 install Django==1.11'
 end
@@ -65,9 +90,9 @@ execute 'django_db_migrate' do
 	command 'python3 manage.py migrate'
 end
 
-execute 'start_redis_server' do
-	command 'docker run -p 6379:6379 -d redis:2.8'
-end
+# execute 'start_redis_server' do
+# 	command 'docker run -p 6379:6379 -d redis:2.8'
+# end
 
 # execute 'start_django_server' do
 # 	cwd '/home/vagrant/project/'
