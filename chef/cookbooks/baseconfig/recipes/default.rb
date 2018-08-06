d25bf713cb7f3b6f96f7e6c2175ee8f7f80e1ddb
zhaohuis <zhaohuis@sfu.ca>
2018-08-05 21:49:06 -0700

    Production-ready Nginx+PostgreSQL Deploy

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,27 +1,56 @@
 # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
 cookbook_file "apt-sources.list" do
-	path "/etc/apt/sources.list"
+  path "/etc/apt/sources.list"
 end
 execute 'apt_update' do
-	command 'apt-get update'
+  command 'apt-get update'
 end
+
 
 # Base configuration recipe in Chef.
 package "wget"
 package "ntp"
-package "curl"
-package "python3-pip"
-package "python3-dev"
-package "nodejs"
-# package "postgresql-server-dev-all"
-# package "libpython-dev"
-# package "libpq-dev"
-
 cookbook_file "ntp.conf" do
 	path "/etc/ntp.conf"
 end
 execute 'ntp_restart' do
 	command 'service ntp restart'
+end
+package "curl"
+package "python3-pip"
+package "python3-dev"
+package "nodejs"
+
+
+
+#Postgres
+package "postgresql"
+execute 'postgres_user' do
+	command 'echo "CREATE DATABASE mydb; CREATE USER vagrant; GRANT ALL PRIVILEGES ON DATABASE mydb TO vagrant;" | sudo -u postgres psql'
+end
+
+# nginx
+package "nginx"
+cookbook_file "nginx-default" do
+	path "/etc/nginx/sites-available/default"
+end
+service "nginx" do
+	action :restart
+end
+
+# Django Deploy mode
+package "postgresql-server-dev-all"
+package "libpython3-dev"
+execute 'install django uwsgi' do
+	command 'pip3 install django psycopg2 uwsgi'
+end
+
+
+
+
+#### -------------project dependencies---------------
+execute 'install_django' do
+	command 'pip3 install Django==1.11'
 end
 
 execute 'install_dependencies' do
@@ -32,10 +61,6 @@ execute 'install_dependencies' do
 	jieba==0.38 \
 	Whoosh==2.7.4 \
 	beautifulsoup4'
-end
-
-execute 'install_django' do
-	command 'pip3 install Django==1.11'
 end
 
 execute 'install_channels' do
@@ -59,6 +84,14 @@ end
 
 package "docker-ce"
 
+
+execute 'django_db_makemigrations' do
+	user 'vagrant'
+	cwd '/home/vagrant/project/'
+	command 'python3 manage.py makemigrations'
+end
+
+
 execute 'django_db_migrate' do
 	user 'vagrant'
 	cwd '/home/vagrant/project/'
@@ -68,6 +101,29 @@ end
 execute 'start_redis_server' do
 	command 'docker run -p 6379:6379 -d redis:2.8'
 end
+
+
+execute 'fixture' do
+	user 'vagrant'
+	cwd '/home/vagrant/project/'
+	command 'python3 manage.py loaddata initial_data.json'
+end
+
+
+cookbook_file "rc.local" do
+  path "/home/vagrant/project/chef/cookbooks/baseconfig/files/default/rc.local"
+end
+execute 'startup' do
+	command '/usr/local/bin/uwsgi --ini /home/vagrant/project/uwsgi.ini --daemonize /var/log/blogproject.log'
+end
+
+
+execute "stat_collected" do
+	cwd '/home/vagrant/project'
+	command 'python3 manage.py collectstatic --noinput'
+end
+
+
 
 # execute 'start_django_server' do
 # 	cwd '/home/vagrant/project/'
